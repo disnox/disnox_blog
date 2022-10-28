46e8b0e229a5f460c13bed2435968ea9d2d3e5cc
NOX <45273526@qq,com>
2022-10-28 22:58:12 +0800

    update

--- data/project.js
+++ data/project.js
@@ -2,7 +2,7 @@ exports.projects = [
   {
     title: 'copy',
     description: '基于Docusaurus v2 静态网站生成器实现个人博客',
-    preview: 'https://www.helloimg.com/image/ZVboJh',
+    preview: 'https://www.helloimg.com/images/2022/10/28/ZV9td9.png',
     website: 'https://www.disnox.top/',
     source: 'https://github.com/disnox/disnox_blog',
     tags: ['opensource', 'design', 'favorite'],
